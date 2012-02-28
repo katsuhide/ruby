2113e8d63d9b48f631413be139c7d0ad1ae540e2
katsuhide <kamonegi.gg@gmail.com>
2012-02-28 15:21:02 +0900

    リファクタ

--- vola/vola.rb
+++ vola/vola.rb
@@ -1,57 +1,23 @@
-# 渡された配列の標準偏差を算出するクラス
-#class Vola
+# 渡された配列の標準偏差を算出するようにArrayを拡張
 class Array
-  def sum_with_number
-    s = 0.0
-    n = 0
-    self.each do |v|
-      next if v.nil?
-      s += v.to_f
-      n += 1
+    # 要素の合計を算出する
+    def sum
+        inject(0.0){|r,i| r+=i.to_f}    
     end
-    [s, n]
-  end
-  
-  def sum
-    s, n = self.sum_with_number
-    s
-  end
-  
-  def avg
-    s, n = self.sum_with_number
-    s / n
-  end
-  alias mean avg
-  
-  def var
-    c = 0
-    while self[c].nil?
-      c += 1
+    def avg
+        inject(0.0){|r,i| r+=i.to_f }/size
     end
-    mean = self[c].to_f
-    sum = 0.0
-    n = 1
-    (c+1).upto(self.size-1) do |i|
-      next if self[i].nil?
-      sweep = n.to_f / (n + 1.0)
-      delta = self[i].to_f - mean
-      sum += delta * delta * sweep
-      mean += delta / (n + 1.0)
-      n += 1
+    # 要素の平均を算出する
+    def avg
+        inject(0.0){|r,i| r+=i.to_f }/size
     end
-    sum / n.to_f
-  end
-  
-  def stddev
-    Math.sqrt(self.var)
-  end
-
+    # 要素の分散を算出する
+    def variance
+        a = avg
+        inject(0.0){|r,i| r+=(i.to_f-a)**2 }/size
+    end
+    # 要素の標準偏差を算出する
+    def stddev
+        Math.sqrt(variance)
+    end
 end
-
-#a = Vola.new()
-a = [31 ,29, 29, 30, 32, 33, 32, 31, 26, 28, 31,nil]
-
-puts a.sum
-puts a.avg
-puts a.var
-puts a.stddev
